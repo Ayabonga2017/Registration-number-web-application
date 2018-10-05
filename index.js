ba21955184f0a611f01876f6658ee32974b972d2
coder <nompetsheniayabonga@gmail.com>
2018-10-05 14:39:14 +0200

    added values to my registration table

--- index.js
+++ index.js
@@ -5,63 +5,43 @@ const exphbs = require('express-handlebars');
 const Registration = require('./logic');
 const flash = require('express-flash');
 const session = require('express-session');
-
-// initialise session middleware - flash-express depends on it
-app.use(session({
-  secret: "this line for an error message",
-  resave: false,
-  saveUninitialized: true
-}));
+const pg = require("pg");
+const Pool = pg.Pool;
 
 // initialise the flash middleware
 app.use(flash());
-
 app.use(express.static('public'));
-app.set('view engine', 'handlebars');
-app.engine('handlebars', exphbs({
-  defaultLayout: 'main'
-}));
 
+app.set('view engine', 'handlebars');
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.use(bodyParser.json());
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({
-  extended: false
-}))
-const pg = require("pg");
-const Pool = pg.Pool;
+app.use(bodyParser.urlencoded({extended: false}))
+
+// initialise session middleware - flash-express depends on it
+app.use(session({ secret: "this line for an error message",
+  resave: false,
+  saveUninitialized: true
+}));
 // should we use a SSL connection
 let useSSL = false;
 let local = process.env.LOCAL || false;
-if (process.env.DATABASE_URL && !local) {
-  useSSL = true;
-}
+if (process.env.DATABASE_URL && !local) { useSSL = true;}
 // which db connection to usecoder123
-
 const connectionString = process.env.DATABASE_URL || 'postgresql://coderreg:coder123@localhost:5432/my_regs';
-const pool = new Pool({
-  connectionString,
-  ssl: useSSL
-});
+const pool = new Pool({connectionString,ssl: useSSL});
 
 const RegGreet = Registration(pool);
-
-app.get('/',  async function (req , res){
-
-    res.render('home')
-});
+app.get('/',  async function (req , res){ res.render('home')});
 
 app.post("/registration", async function (req , res , next){
+ try {
+   let regText = req.body.regText;
+   let display = await RegGreet.addRegNumber(regText);
 
-  try {
-    let regText = req.body.regText;
-    console.log(regText)
-    let display = await RegGreet.addRegNumber(regText);
-    
-    res.render('home', {display , regText})
-  } catch (error) {
-    next(error)
-  }
-  
+   res.render('home', {display , regText})
+   } catch (error) {
+    next(error) }
 })
 
 let PORT = process.env.PORT || 20201;
