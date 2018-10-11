092587cb7db459768c0f272413350da97f51eb01
coder <nompetsheniayabonga@gmail.com>
2018-10-11 18:25:02 +0200

    created a delete/reset route and btn to delete all stored regs on my database

--- logic.js
+++ logic.js
@@ -21,16 +21,17 @@ module.exports = function (pool) {
     }
   
     return regNumber;
-  } 
+  }; 
+
   async function showRegs(){
 
     await pool.query('select * from registrationNumber')
-} 
+};
 
   async function deleteRegs(){
 
     await pool.query('delete from registrationNumber')
-} 
+}; 
 
   async function  check(initial){
     duplicateCheck = await pool.query('select registrations from registrationNumber where registrations= $1' ,[initial])
@@ -38,47 +39,10 @@ module.exports = function (pool) {
       return duplicateCheck.rowCount === 1;
     }
 
-    //Filter : filters out unwanted elements
-    async function filter(values) { var searchdata = [];
-      var numberPlates = Object.keys(regNumberMap);
-      if (values != '') { for (var i = 0; i < numberPlates.length; i++) {
-          if (numberPlates[i].startsWith(values)) { searchdata.push(numberPlates[i]);
-          }
-        }
-      }
-      return searchdata;
-    }
-  
-    //filter function for towns that startsWith('').....
-    async function townFilter(town){ var cpt = []; var malmas = [];
-      var bellv =[]; var George= []; var All= [];
-  
-    if (town === "CA ") {
-      for( var key in regNumberMap){ if (key.startsWith("CA ")) {cpt.push(key);}}
-      return cpt;
-    }
-  
-    if (town === "CK") {
-      for( var key in regNumberMap) { if (key.startsWith("CK")) { malmas.push(key); }}
-      return malmas;
-    }
-  
-    if (town === "CY") { for( var key in regNumberMap) {
-        if (key.startsWith("CY")) {bellv.push(key); }}
-      return bellv; }
-  
-    if (town === "CAW") { for( var key in regNumberMap) { if (key.startsWith("CAW")) {George.push(key); }}
-      return George;
-    }
-      if (town === "") { for( var key in regNumberMap) { if (key.startsWith("")) { All.push(key); }}
-        return All;
-      }
-    }
-  
+    
     return {
       addRegNumber,
       filter,
-      townFilter,
       check,
       showRegs,
       deleteRegs
