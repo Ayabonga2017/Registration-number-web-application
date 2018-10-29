a252443e731eb8e3d91503af5acc597f0d00977d
coder <nompetsheniayabonga@gmail.com>
2018-10-29 13:19:56 +0200

    added flash messages

--- index.js
+++ index.js
@@ -14,83 +14,79 @@ app.use(flash());
 app.use(express.static('public'));
 
 app.set('view engine', 'handlebars');
-app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.use(bodyParser.json());
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.urlencoded({ extended: false }))
 
 // initialise session middleware - flash-express depends on it
-app.use(session({ secret: "this line for an error message",
+app.use(session({
+  secret: "this line for an error message",
   resave: false,
   saveUninitialized: true
 }));
 // should we use a SSL connection
 let useSSL = false;
 let local = process.env.LOCAL || false;
-if (process.env.DATABASE_URL && !local) { useSSL = true;}
+if (process.env.DATABASE_URL && !local) { useSSL = true; }
 // which db connection to coderreg
-const connectionString = process.env.DATABASE_URL || "postgres://coderreg:coder123@localhost:5432/my_regs";
+const connectionString = process.env.DATABASE_URL || "postgres://coderregi:coder123@localhost:5432/my_regi";
 // var conString = 
 
-const pool = new Pool({connectionString,ssl: useSSL});
+const pool = new Pool({ connectionString, ssl: useSSL });
 
 const RegdBase = Registration(pool);
-app.get('/',  async function (req , res){ res.render('home')});
+app.get('/', async function (req, res) { res.render('home') });
 
-app.post("/reg_numbers", async function (req , res , next){
- try {
-  
-   let regText = req.body.regText;
-   let display = await RegdBase.addRegNumber(regText);
-   console.log(display)
-   let regcheck = await RegdBase.check(regText)
-  //  duplicate = duplicate.length;
+app.post("/reg_numbers", async function (req, res, next) {
+  try {
 
-if (regText  === "" || regText === undefined){
+    let regText = req.body.regText;
 
-  req.flash("info", ' PLEASE ENTER A VALID REGISTRATION IN CAPS (eg. CA .., CK .., CY .. , CAW ..)')
-}else if (regcheck === 0) {
-  
-  req.flash("info", 'reg exists , please enter a new reg ')
-}else if (regcheck === 1) {
-  
-  req.flash("info", 'reg is aduplicate ,  enter a new one ')
-}
-   res.render('home', {display , regText})
-   } catch (error) {
-    next(error) }
+    let regcheck = await RegdBase.check(regText)
+
+    if (regText === "" || regText === undefined) {
+
+      req.flash("info", ' PLEASE ENTER A VALID REGISTRATION IN CAPS (eg. CA .., CK .., CY .. , CAW ..)')
+    } else if (regcheck === 0) {
+
+      req.flash("info", 'reg exists , please enter a new reg ')
+    } else if (regcheck === 1) {
+
+      req.flash("info", 'reg is aduplicate ,  enter a new one ')
+    }
+    let display = await RegdBase.addRegNumber(regText);
+    res.render('home', { display, regText })
+  } catch (error) {
+    next(error)
+  }
 })
 
 app.post('/reset', async function (req, res, next) {
 
   try {
     let reset = await RegdBase.deleteRegs();
-    
-  res.render("home", { reset })
+
+    res.render("home", { reset })
   } catch (error) {
     next(error)
   }
 })
 
-// app.get('/reg_numbers', function (req, res) {
-//   req.flash('info', 'Flash Message Added');
-//   res.redirect('/');
-// });
+app.get('/filter:regText', async function (req, res, next) {
+  try {
+    let registrations = req.body.regText;
+    let registration_list = await RegdBase.check(registrations);
 
-// app.get('/filter:regText', async function (req, res, next) {
-//   try {
-//     let registrations = req.body.regText;
-//     let registration_list = await RegdBase.check(registrations);
-  
-    
-//   res.render("home", {registration_list , registrations })
-//   } catch (error) {
-//     next(error)
-//   }
-// })
 
-let PORT = process.env.PORT || 1321;
+    res.render("home", { registration_list, registrations })
+  } catch (error) {
+    next(error)
+  }
+})
 
-app.listen(PORT, function(){
+let PORT = process.env.PORT || 1111;
+
+app.listen(PORT, function () {
   console.log('App starting on port', PORT);
 });
--- logic.js
+++ logic.js
@@ -1,60 +1,56 @@
 module.exports = function (pool) {
   // remember the initialize regNumberMap from storeRegNumbers - to support localStorage vertical
-  
+
   async function addRegNumber(regNumber) {
-  // console.log(regNumber)
-  var initial = regNumber.split(" ")[0].trim();
-  
-   let regs = await pool.query('select id from towns where reginitial = $1',[initial])
-   console.log(regs, initial, regNumber);
-   
-   if(regs.rowCount == 0) {
-  
-    // return 'PLEASE ENTER A VALID REGISTRATION IN CAPS (eg. CA .., CK .., CY .. , CAW ..)' ;
-    await pool.query('insert into registrationNumber  (town_id, registrations) values($1 , $2)',[regs.rows[0].id, regNumber])
-
-   } else if (regs.rowCount == 1) {
-    return "already exists"
-
-   }
-  }; 
-//  async function allregs(){
-
-//   return regNumber;
-//  }
-   //Filter : filters out unwanted elements
-   async function filter(values) { var searchdata = [];
-    var numberPlates = Object.keys(regNumberMap);
-    if (values != '') { for (var i = 0; i < numberPlates.length; i++) {
-        if (numberPlates[i].startsWith(values)) { searchdata.push(numberPlates[i]);
-        }
-      }
-    }
-    return searchdata;
-  }
-
-  async function showRegs(){
 
  
-};
+    let reginitial = regNumber.split(" ",1).join();
+   
+    //check if town is valid
+    let towntags = await pool.query('select id from towns where reginitial = $1', [reginitial])
 
-  async function deleteRegs(){
+    console.log(towntags)
+    // //check if registration exists on my table
+    if (towntags.rowCount == 1) {
+      let regs = await pool.query('select * from registrationNumber where registrations= $1', [regNumber])
+  
+    //if not it should insert
+    if (regs.rowCount == 0) {
+
+      await pool.query('insert into registrationNumber (town_id, registrations) values($1 , $2)', [towntags.rows[0].id, regNumber])
+      console.log(regs)
+    }
+    else if (regs.rowCount >0) {
+      return "already exists"
+
+    }
+   
+    return regNumber;
+  }
+  }
+
+  async function showRegs() {
+
+    let regs = await pool.query('select registrations from towns ');
+    return regs.rows;
+  };
+
+  async function deleteRegs() {
 
     await pool.query('delete from registrationNumber')
-}; 
+  };
 
-  async function  check(regNumber){
-    regs = await pool.query('select * from registrationNumber where registrations= $1' ,[regNumber])
-    
-      return regs.rows;
-    }
+  async function check(regNumber) {
+    regs = await pool.query('select * from registrationNumber where registrations= $1', [regNumber])
 
-    
-    return {
-      addRegNumber,
-      filter,
-      check,
-      showRegs,
-      deleteRegs
-    }
+    return regs.rows;
   }
+
+
+  return {
+    addRegNumber,
+    check,
+    showRegs,
+    deleteRegs
+  }
+}
