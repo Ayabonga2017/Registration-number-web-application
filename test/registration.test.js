b6351cec1dcc74832eccdfd9ea9ea8a0e72844c4
coder <nompetsheniayabonga@gmail.com>
2018-11-05 13:03:57 +0200

    added more tests

--- test/registration.test.js
+++ test/registration.test.js
@@ -4,7 +4,7 @@ const pg = require("pg");
 const Pool = pg.Pool;
 
 // we are using a special test database for the tests
-const connectionString = process.env.DATABASE_URL || 'postgresql://coderegi:coder123@localhost:5432/my_registration';
+const connectionString = process.env.DATABASE_URL || 'postgresql://postgres@localhost:5432/registration_tests';
 
 const pool = new Pool({
     connectionString
